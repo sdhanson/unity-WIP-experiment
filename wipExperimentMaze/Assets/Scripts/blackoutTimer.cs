80cd9504250c585eb4238e75e33bc72e1fb2449d
weretere <38572760+weretere@users.noreply.github.com>
2018-08-01 12:24:49 -0500

    Fixed Timer

--- wipExperimentMaze/Assets/Scripts/blackoutTimer.cs
+++ wipExperimentMaze/Assets/Scripts/blackoutTimer.cs
@@ -8,100 +8,63 @@ public class blackoutTimer : MonoBehaviour {
 	public Camera blackout;
 
 	private float velocity;
-	private bool walkingState_normal;
-	private bool walkingState_blackout;
-	private bool walkingState_undoBlackout;
+	private static int walkingState_waiting = 0;
+	private static int walkingState_normal = 1;
+	private static int walkingState_blackout = 2;
+	private static int walkingState_waiting2 = 3;
+	private static int walkingState_undoBlackout = 4;
+	private int walkingState = walkingState_waiting;
+	private float minuteTimer = 0;
+	private float secondTimer = 0;
+
 	private List<float> timeList = new List<float> ();
 
 	// Use this for initialization
 	void Start () 
 	{
-		walkingState_normal = true;
-		walkingState_blackout = false;
 		velocity = AccelerometerInput4.velocity;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		walkingState ();
+		velocity = AccelerometerInput4.velocity;
+		walkingStateMachine ();
 	}
 
-	void walkingState()
+	void walkingStateMachine()
 	{
-		if((walkingState_normal) && (velocity != 0f))
-		{
-			timeList.Add (Time.time);
-
-			if(velocity == 0f)
-			{
-				timeList.Add(Time.time);
+		if (walkingState == walkingState_waiting) {
+			if (velocity != 0f || Input.GetMouseButtonDown (0)) {
+				walkingState = walkingState_normal;
+				Debug.Log ("normal");
+				minuteTimer = Time.time;
 			}
-
-			if((timeList[timeList.Count - 1] - timeList[0]) >= 60f && gateCollider.isInGate == "1")
-			{
-				System.Threading.Thread.Sleep (1000);
-				walkingState_normal = false;
-				walkingState_blackout = true;
+		} else if (walkingState == walkingState_normal) {
+			if (minuteTimer + 6 < Time.time && !(Input.GetMouseButton (0))) {
+				walkingState = walkingState_blackout;
+				Debug.Log ("blackout");
+				secondTimer = Time.time;
 			}
-			return;
+		} else if (walkingState == walkingState_blackout) {
+			if (secondTimer + 1 < Time.time) {
+				main.gameObject.SetActive (false);
+				blackout.gameObject.SetActive (true);
+				walkingState = walkingState_waiting2;
+			}
+		} else if (walkingState == walkingState_waiting2) {
+			if (Input.GetMouseButtonUp (0)) {
+				walkingState = walkingState_undoBlackout;
+				main.gameObject.SetActive (true);
+				blackout.gameObject.SetActive (false);
+				secondTimer = Time.time;
+			}
+		} else if (walkingState == walkingState_undoBlackout) {
+			if (secondTimer + 1 < Time.time) {
+				walkingState = walkingState_waiting;
+			}
 		}
-
-		if(walkingState_blackout)
-		{
-			main.enabled = false;
-			blackout.enabled = true;
-
-			if(Input.GetMouseButton(0)) //Don't know whether this will need to be a 1 or a 0 ***Just did true for now***
-			{
-				main.enabled = true;
-				blackout.enabled = false;
-				walkingState_blackout = false;
-				walkingState_normal = true;
-				timeList.Clear ();
-			}
-			return;
-		}
-	}
-
-
-	void walkingStateTest()
-	{
-		if(walkingState_normal)
-		{
-			timeList.Add (Time.time);
-
-			Debug.Log (timeList[timeList.Count - 1] - timeList[0]);
-
-			if(velocity == 0f)
-			{
-				timeList.Add(Time.time);
-			}
-
-			if((timeList[timeList.Count - 1] - timeList[0]) >= 16f && gateCollider.isInGate == "1")
-			{
-				System.Threading.Thread.Sleep (1000);
-				walkingState_normal = false;
-				walkingState_blackout = true;
-			}
-			return;
-		}
-
-		else if(walkingState_blackout)
-		{
-			main.enabled = false;
-			blackout.enabled = true;
-
-			if(Input.GetKey("up")) //Don't know whether this will need to be a 1 or a 0 ***Just did true for now***
-			{
-				main.enabled = true;
-				blackout.enabled = false;
-				walkingState_blackout = false;
-				walkingState_normal = true;
-				timeList.Clear ();
-			}
-			return;
-		}
+			
 	}
 
 }
